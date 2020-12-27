8b5c8dc448a46f631a7c2aaa80eeb00cd798473f
Galal Ahmed <islam.galal212@gmail.com>
2020-12-27 19:34:26 +0100

    added Database and repositories

---
+++ src/main/java/com/example/application/data/repository/RezeptRepository.java
@@ -0,0 +1,26 @@
+package com.example.application.data.repository;
+
+import com.example.application.data.entity.Rezept;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+public interface RezeptRepository extends JpaRepository<Rezept, Integer> {
+
+    @Query(value = "select * from Rezept offset :offset rows fetch next :limit ROWS ONLY", nativeQuery = true)
+    List<Rezept> getRezeptePage(int offset, int limit);
+
+    @Query("select r from Rezept r where r.creator.id=:creatorId")
+    List<Rezept> getUserRezepte(Integer creatorId);
+
+    @Query("select count(r) from Rezept r where r.creator.id=:creatorId")
+    Integer countOfRezepte(Integer creatorId);
+
+    @Transactional
+    @Modifying
+    @Query("update Rezept r set r.rezeptName = :name, r.inhalt = :inhalt where r.id = :rezeptId")
+    void setRezeptInfoById(Integer rezeptId, String name, String inhalt);
+}
