139e86c1e540af73d741ea378b75ea9e9257e0ee
Mahmoud Barakat <Mahmoud.Barakat@student.htw-berlin.de>
2021-01-17 17:22:23 +0100

    DB-TODO

---
+++ src/main/java/com/example/application/data/Repos/SeriesRepo.java
@@ -0,0 +1,8 @@
+package com.example.application.data.Repos;
+
+import com.example.application.data.entity.Series;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SeriesRepo extends JpaRepository<Integer, Series> {
+    //  TODO
+}
